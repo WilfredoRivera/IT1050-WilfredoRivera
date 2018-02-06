9e3bd8966298f366f5d958c6c35d600310b88300
WilfredoRivera <31965186+WilfredoRivera@users.noreply.github.com>
2018-02-06 15:21:48 -0800

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab01")]
+[assembly: AssemblyTitle("Lab02-WilfredoRivera")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Lab01")]
+[assembly: AssemblyProduct("Lab02-WilfredoRivera")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1983c98-4b23-4d7e-b7d6-f69abf4277fc")]
+[assembly: Guid("12b55fc0-b0a7-4e6e-8a67-55d84a479539")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Program.cs
+++ Program.cs
@@ -4,13 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab01
-{ // Hello From Wilfredo Rivera
+namespace Lab02_WilfredoRivera
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from Wilfredo Rivera");
+            int number01;
+            int number02;
+            int sum;
+            Console.WriteLine("First Integer is  "); 
+            number01 = int.Parse(Console.ReadLine());
+            Console.Write("Second integer is  "); 
+            number02 = int.Parse(Console.ReadLine());
+            sum = number01 + number02;
+            Console.WriteLine($"Sum is {sum}");
+            // Question Number 2
+            int x;
+            int y;
+            int z;
+            int product;
+            Console.WriteLine("x is equal to 15");
+            x = 15;
+            Console.WriteLine("y is equal to 40");
+            y = 40;
+            Console.WriteLine("z is equal to 24");
+            z = 24;
+            Console.WriteLine("what is x+y * z+10");
+            product = (x + y) * (z + 10);
+            Console.WriteLine($"prodcut is {product}");
+            // Question Number 3
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello\nWorld!");
+            Console.WriteLine("\"Hello World!\"");
+            Console.WriteLine("Hello\\World");
+
+            
+
+
+
+
+          
+            
+            
+
+
+
+
+
         }
     }
 }
