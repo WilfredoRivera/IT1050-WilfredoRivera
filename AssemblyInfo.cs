f67d72a48c301d6a90fc5acaeea4b19b462c3563
WilfredoRivera <31965186+WilfredoRivera@users.noreply.github.com>
2018-04-22 14:04:05 -0700

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab09")]
+[assembly: AssemblyTitle("Test1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Lab09")]
+[assembly: AssemblyProduct("Test1")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e4c944e-1225-4def-adf8-5c70b9410181")]
+[assembly: Guid("5531da07-c1da-4683-bea6-e2c831f978e7")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab09
+namespace Test1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int t = 0;
+            while (t++ < 128)
+          Console.WriteLine("Value of t is {{{0}}}", ++t);
+            for (int i = 49; i >= 1;)
+            Console.Write(" {0},", i--);
+            int v = 1;
+            while (v <= 21)
+            Console.Write("{0}   ", v++);
+            int n = 8;
+            int w = 10;
+            while (w < n) ;
+            Console.WriteLine("{0}",w++);
+            // a do while loop executes the code without checking if the code is can be executed or not while a while loop checks the condition before running it.
+            bool icyrain = true;
+            bool tornadoWarning = true;
+            if (icyrain = false && tornadoWarning == false) ;
+            Console.WriteLine("\"lets go outside!\"");
+            bool keeplooping;
+            while (keeplooping = true) ;
+            
+            
+            
         }
     }
 }
